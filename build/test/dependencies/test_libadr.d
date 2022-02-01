7c226009191f1df1f3a42958558270e1dc360e69
Pryscilla123 <pryscillaromao@hotmail.com>
2022-02-01 19:46:07 +0000

    Ignorando a pasta build.

--- build/test/dependencies/test_libadr.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_libadr.o: test/test_libadr.c \
- /var/lib/gems/2.5.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- /var/lib/gems/2.5.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h \
- src/libadr.h
--- build/test/dependencies/test_libadr_runner.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_libadr_runner.o: \
- build/test/runners/test_libadr_runner.c \
- /var/lib/gems/2.5.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- /var/lib/gems/2.5.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h
